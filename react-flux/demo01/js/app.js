54e274c9a56ad4159b81222ffe17bf34c62272d1
Ma Junbao <mjbwork12@gmail.com>
2016-12-06 17:38:15 +0800

    add react-flux

---
+++ react-flux/demo01/js/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CourseApp from './components/CourseApp.react';
+
+ReactDOM.render(
+  <CourseApp />,
+  document.getElementById('courseApp')
+)
---
+++ react-flux/demo01/js/components/CourseApp.react.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './header/Header.react';
+import Button from './uikit/Button';
+
+function App(){
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App;
