181c08b93671e223edddf9cd2cd0850c74360006
ma junbao <mjbwork12@gmail.com>
2016-12-07 19:49:07 +0800

    init

---
+++ react-flux/demo01/js/components/App.react.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, Icon, Select, Text} from './UIKit';
+
+
+function App(){
+  return (
+    <div>
+      <Button primary>知道</Button>
+      <Button success>我怎么</Button>
+    </div>
+  )
+}
+
+export default App;
---
+++ react-flux/demo01/js/components/UIKit.js
@@ -0,0 +1,11 @@
+import Button from './UIKit/Button';
+import Icon from './UIKit/Icon';
+import Select from './UIKit/Select';
+import Text from './UIKit/Text';
+
+export {
+  Button,
+  Icon,
+  Select,
+  Text
+}
