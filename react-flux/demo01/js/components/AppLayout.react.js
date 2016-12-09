d24f40f43f20445986ed2b4c94cd9bfa0fa6b5f4
Ma Junbao <mjbwork12@gmail.com>
2016-12-09 17:14:07 +0800

    init

--- react-flux/demo01/js/components/AppLayout.react.js
+++ react-flux/demo01/js/components/AppLayout.react.js
@@ -3,30 +3,47 @@ import React from 'react';
 class AppLayout extends React.Component {
   contructor(props) {
     supper(props);
+    this.Toolbar;
+    this.Navigator;
+    this.Canvas;
+    this.Inspector;
   }
 
-  componentDidMount(){
-    let header,section,article;
-    console.log(React.Children.toArray(this.props.children))
-    console.log(React.Children.only(this.props.children[0]))
+  componentWillMount(){
     React.Children.forEach(this.props.children, children => {
-      if(children.type.name=="Navigator"){
-        
+      let childrenName = children.type.toString().match(/[A-Z][a-z]*/).toString();
+      if(childrenName == 'Toolbar'){
+        this.Toolbar = children
+      }else if(childrenName == 'Navigator') {
+        this.Navigator = children
+      }else if(childrenName == 'Canvas') {
+        this.Canvas = children
+      }else if(childrenName == 'Inspector') {
+        this.Inspector = children
       }
     })
   }
 
   render(){
     return(
-      <div>
-        {this.props.children}
+      <div className="app-layout">
+        <div className="app-layout-top">
+          {this.Toolbar}
+        </div>
+        <div className="app-layout-main">
+          <div className="app-layout-left">
+            {this.Navigator}
+          </div>
+          <div className="app-layout-center">
+            {this.Canvas}
+          </div>
+          <div className="app-layout-right">
+            {this.Inspector}
+          </div>
+        </div>
       </div>
     )
   }
 }
-class LayoutLeft extends React.Component {}
-class LayoutRight extends React.Component {}
-class LayoutTop extends React.Component {}
-class LayoutCenter extends React.Component {}
 
 export default AppLayout;
