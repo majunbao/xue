1949c3f7be9bd0bcfd352d2b373a1dd58516c020
Ma Junbao <mjbwork12@gmail.com>
2017-02-09 07:26:08 +0800

    demo07

---
+++ ts/demo07/src/draw.tsx
@@ -0,0 +1,34 @@
+let XY = (e) => {
+  console.log(e.offsetX, e.layerX);
+}
+
+let style = {
+  width: 600,
+  height: 600,
+  background: '#def',
+  position: 'absolute',
+  left: 300,
+  top: 20
+}
+
+let spanStyle = {
+  width: 200,
+  height: 200,
+  background: '#edf',
+  position: 'absolute',
+  left: 0,
+  top: 50
+}
+
+let spanEvent = (e) => {
+  e.stopPropagation();
+  console.log(e.offsetX, e.layerX);
+}
+
+function Canvas() {
+  return <div style={style} onMouseDown={XY}>
+    <span style={spanStyle} onMouseDown={spanEvent}>nihao</span>
+  </div>
+}
+
+export default Canvas;
---
+++ ts/demo07/src/root.tsx
@@ -0,0 +1,8 @@
+import {render} from 'inferno';
+import Canvas from './draw';
+
+const container = document.getElementById('root');
+
+
+render(<Canvas />, container);
+
