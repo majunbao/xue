1cd7af1bf537c45e84a178029dde56f348e33583
majunbao <mjb410641829@163.com>
2020-05-27 01:15:46 +0800

    a

--- js/gestures/01.js
+++ js/gestures/01.js
@@ -4,14 +4,25 @@ document.addEventListener('DOMContentLoaded', function(ev){
   var kbs = [];
   var _XYul = document.getElementById('xy');
   var _KBul = document.getElementById('kb');
+  var canvas = document.getElementById('canvas');
+  var ctx=canvas.getContext("2d");
 
   // 按下
   var onTouchStart = function(e){
+    var x = e.touches[0].pageX;
+    var y = e.touches[0].pageY;
+
     clearRecordXYs();
     clearRecordKBs();
     _clearXY();
     _clearKB();
     document.addEventListener('touchmove', onTouchMove, false);
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "red";
+    
   }
   // 移动
   var onTouchMove = function(e){
@@ -21,11 +32,15 @@ document.addEventListener('DOMContentLoaded', function(ev){
     recordKBs();
     _renderXY(x, y);
     if(kbs.length>0){_renderKB(kbs[0].k, kbs[0].b);}
-    
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
   }
   // 抬起
   var onTouchEnd = function(e){
     document.removeEventListener('touchmove', onTouchMove);
+
+    
   }
 
   // 触发
@@ -35,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function(ev){
 
   // 求斜率k，带入x1，x2 k=(y2-y1)/(x2-x1)
   function mathKB (xys){
-    var k, b, b1, b2;
+    var k, b;
     
     if(xys.length>1) {
       var x1 = xys[0].x;
@@ -44,15 +59,29 @@ document.addEventListener('DOMContentLoaded', function(ev){
       var y2 = xys[1].y;
 
       if(x1 == x2) {
-        k = 0;
+        k = 1;
       } else {
         k = (y2-y1) / (x2-x1);
+        // k = parseFloat(k.toFixed(3));
       }
 
-      b1 = y1 - k * x1;
-      b2 = y2 - k * x2;
-      b = b1 + ',' +  b2;
+      b = y1 - k * x1;
+      // b = parseFloat(b.toFixed(3));
 
+      // if(xys.length > 2) {
+      //   var x3 = xys[2].x;
+      //   var y3 = xys[2].y;
+
+      //   var py = x3 * k + b;
+      //   var value = y3 - py;
+
+      //   if(value >- 0.01 && value < 0.01) {
+      //     console.log(true)
+      //   }else {
+      //     console.log(false)
+      //   }
+      // }
+      
       return {
         k: k,
         b: b
@@ -88,7 +117,8 @@ document.addEventListener('DOMContentLoaded', function(ev){
   
   function _renderXY(x, y) {
     var li = document.createElement('li');
-    li.innerText = xys.length + ' pageX: ' + x + ' pageY: ' +  y + '\n';
+    li.innerText = ' x: ' + x + ' y: ' +  y + '\n';
+    // li.innerText = xys.length + ' x: ' + x + ' y: ' +  y + '\n';
 
     _XYul.insertBefore(li, _XYul.firstChild);
   }
@@ -100,7 +130,8 @@ document.addEventListener('DOMContentLoaded', function(ev){
 
   function _renderKB(k, b) {
     var li = document.createElement('li');
-    li.innerText = kbs.length + ' K: ' + k + ' B: ' +  b + '\n';
+    li.innerText = ' k: ' + k + ' b: ' +  b + '\n';
+    // li.innerText = kbs.length + ' k: ' + k + ' b: ' +  b + '\n';
 
     _KBul.insertBefore(li, _KBul.firstChild);
   }
@@ -109,4 +140,6 @@ document.addEventListener('DOMContentLoaded', function(ev){
     i = 0;
     _KBul.innerText = '';
   }
+
+  function _canvas() {}
 })
